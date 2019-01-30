cd18dd3b0e1057818d171a66868d0fc6299c1b2e
Timur Gladkikh <tgladkikh@freshbooks.com>
2019-01-30 13:18:24 -0500

    Cleaned up the code. Added more logic to the script

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import json
 import logging
 from pathlib import Path
 import uuid
+import os
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
@@ -12,6 +13,8 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
+
+STATE_FILE = 'setup_state.json'
 
 
 def aws_client(profile, service='s3'):
@@ -23,26 +26,54 @@ def aws_client(profile, service='s3'):
 def cleanup(saved_state):
     logger.info('Clean up...')
     with open(saved_state) as infile:
-        state = json.loads(infile.read())
-        client = aws_client()
+        for line in infile:
+            state = json.loads(line)
+            client = aws_client(state['profile'])
+            response = client.delete_objects(
+                            Bucket=state['bucket'],
+                            Delete={
+                                'Objects': [
+                                    {
+                                        'Key': '*',
+                                        'VersionId': '*'
+                                    },
+                                ],
+                                'Quiet': False
+                            }
+                        )
+            logger.debug(response)
+
+            response = client.delete_bucket(
+                            Bucket=state['bucket']
+                        )
+            logger.debug(response)
 
 
-def enable_replicaton(source, dest):
-    pass
+def append_record(record):
+    with open(STATE_FILE, 'a') as f:
+        json.dump(record, f)
+        f.write(os.linesep)
 
 
 class Bucket:
 
     source_account = ''
+    dest_account = ''
 
     def __init__(self, name, region, profile, lifecycle_rules, iam, bucket_policy):
-        self.bucket = name + '-' + uuid.uuid4().hex
+        self.bucket = {
+            'name': name,
+            'arn': ''
+        }
         self.region = region
         self.profile = profile
         self.lifecycle_rules = lifecycle_rules
         self.iam = {
-            'role_name': 'S3CrossAccountReplicationIamRole' + '-' + uuid.uuid4().hex,
-            'role': iam
+            'role_name': 'S3CrossAccountReplicationIamRole' + '-' + uuid.uuid4().hex[:8],
+            'role_arn': '',
+            'policy_arn': '',
+            'policy': iam,
+            'policy_name': 'S3CrossAccountReplicationIamPolicy' + '-' + uuid.uuid4().hex[:8]
         }
         self.bucket_policy = bucket_policy
         self.save_state()
@@ -50,24 +81,23 @@ class Bucket:
 
     def create_bucket(self):
         logger.info('Creating {0} bucket...'.format(self.bucket))
-        self.client.create_bucket(
+        response = self.client.create_bucket(
             ACL='private',
-            Bucket=self.bucket,
+            Bucket=self.bucket['name'],
             CreateBucketConfiguration={
                 'LocationConstraint': self.region
             }
         )
+        self.bucket['arn'] = 'arn:aws:s3:::' + self.bucket['name']
+        logger.debug('Response: ', response)
 
     def save_state(self):
-        with open('setup_state.json', 'a+') as outfile:
-            feeds = json.loads(outfile)
-            feeds.append(self.__dict__)
-            json.dump(feeds, outfile, ensure_ascii = False)
+        append_record(self.__dict__)
 
     def enable_versioning(self):
         logger.info('Enabling versioning for {0} bucket'.format(self.bucket))
         self.client.put_bucket_versioning(
-                Bucket = self.bucket,
+                Bucket = self.bucket['name'],
                 VersioningConfiguration = {
                     'Status': 'Enabled'
                 }
@@ -76,33 +106,92 @@ class Bucket:
     def enable_lifecycle_policy(self):
         logger.info('Enabling lifecycle policy for {0} bucket'.format(self.bucket))
         self.client.put_bucket_lifecycle_configuration(
-                Bucket = self.bucket,
+                Bucket = self.bucket['name'],
                 LifecycleConfiguration = {
                     'Rules': self.lifecycle_rules
                 }
         )
 
+    def enable_replication(self, destination_arn):
+        logger.info('Adding Replication for {0} bucket'.format(self.bucket))
+        self.client = aws_client(self.profile, 's3')
+        response = self.client.put_bucket_replication(
+                Bucket = self.bucket['name'],
+                ReplicationConfiguration = {
+                    'Role': self.iam['role_arn'],
+                    'Rules': [
+                        {
+                            'ID': 'ReplicationConfiguration' + '-' + uuid.uuid4().hex[:8],
+                            'Prefix': '*',
+                            'Status': 'Enabled',
+                            'Destination': {
+                                'Bucket': destination_arn,
+                                'Account': Bucket.dest_account,
+                                'StorageClass': 'STANDARD',
+                                'AccessControlTranslation': {
+                                    'Owner': 'Destination'
+                                }
+                            }
+                        }
+                    ]
+                }
+        )
+        logger.debug('Response: ', response)
+
+    def create_policy(self):
+        logger.info('Adding IAM policy for {0} profile'.format(self.profile))
+        response = self.client.create_policy(
+                PolicyName = self.iam['policy_name'],
+                Path = '/service-role/',
+                PolicyDocument = json.dumps(self.iam['policy']),
+                Description = 'S3 Cross-Account Replication IAM Policy'
+        )
+        self.iam['policy_arn'] = response['Policy']['Arn']
+        logger.debug('Response: ', response)
+
     def create_iam_role(self):
         logger.info('Adding IAM role for {0} profile'.format(self.profile))
         self.client = aws_client(self.profile, 'iam')
-        self.client.create_role(
+        self.create_policy()
+
+        trust_relationship = {
+          'Version': '2012-10-17',
+          'Statement': [
+            {
+              'Effect': 'Allow',
+              'Principal': {
+                'Service': 's3.amazonaws.com'
+              },
+              'Action': 'sts:AssumeRole'
+            }
+          ]
+        }
+
+        response = self.client.create_role(
             Path='/service-role/',
             RoleName=self.iam['role_name'],
-            AssumeRolePolicyDocument=json.dumps(self.iam['role']),
+            AssumeRolePolicyDocument=json.dumps(trust_relationship),
             Description='S3 Cross-Account Replication IAM Role',
         )
+        self.iam['role_arn'] = response['Role']['Arn']
+        logger.debug('Response: ', response)
+
+        response = self.client.attach_role_policy(
+                RoleName = self.iam['role_name'],
+                PolicyArn = self.iam['policy_arn']
+        )
+        logger.debug('Response: ', response)
 
     def apply_security(self):
         logger.info('Adding policy to {0} bucket'.format(self.bucket))
         self.client.put_bucket_policy(
-                Bucket = self.bucket,
+                Bucket = self.bucket['name'],
                 ConfirmRemoveSelfBucketAccess = False,
                 Policy = json.dumps(self.bucket_policy)
         )
 
 
 def main():
-    # TODO Implement Cleanup logic
     # TODO Implement Continue logic
 
     a = argparse.ArgumentParser()
@@ -116,27 +205,30 @@ def main():
                    help='AWS CLI profile to use for the destination data')
     a.add_argument('--src_region', default='ca-central-1',
                    help='Region for the source bucket. Default is `ca-central-1`')
-    a.add_argument('--dest_region', default='ca-central-1',
+    a.add_argument('--dest_region', default='us-east-2',
                    help='Region for the source bucket. Default is `ca-central-1`')
     a.add_argument('--src_accountid', required=True,
                    help='Account id for the source AWS account')
+    a.add_argument('--dest_accountid', required = True,
+                   help = 'Account id for the destination AWS account')
     a.add_argument('--cleanup', required=False, action='store_true',
                    help = 'Remove existing resources')
     args = a.parse_args()
     logger.debug(args)
 
-    state = 'setup_state.json'
-    if Path(state).is_file():
+    if Path(STATE_FILE).is_file():
         if args.cleanup:
-            cleanup(state)
-        else:
-            logger.info('Cannot continue. Please add `--cleanup` flag and re-run the script...')
-            quit()
-    else:
-        with open(state, mode = 'w', encoding = 'utf-8') as f:
-            f.write(json.dumps([]))
+            cleanup(STATE_FILE)
+            os.remove(STATE_FILE)
+
+        logger.info('Cannot continue. Please add `--cleanup` flag and re-run the script...')
+        quit()
 
     Bucket.source_account = args.src_accountid
+    Bucket.dest_account = args.dest_accountid
+
+    source_bucket = args.source + '-' + uuid.uuid4().hex
+    dest_bucket = args.dest + '-' + uuid.uuid4().hex
 
     source_lifecycle_policy = [
         {
@@ -190,8 +282,8 @@ def main():
                 ],
                 'Effect': 'Allow',
                 'Resource': [
-                    'arn:aws:s3:::{0}'.format(args.source),
-                    'arn:aws:s3:::{0}/*'.format(args.source)
+                    'arn:aws:s3:::{0}'.format(source_bucket),
+                    'arn:aws:s3:::{0}/*'.format(source_bucket)
                 ]
             },
             {
@@ -203,7 +295,7 @@ def main():
                     's3:ObjectOwnerOverrideToBucketOwner'
                 ],
                 'Effect': 'Allow',
-                'Resource': 'arn:aws:s3:::{0}/*'.format(args.dest)
+                'Resource': 'arn:aws:s3:::{0}/*'.format(dest_bucket)
             }
         ]
     }
@@ -226,8 +318,8 @@ def main():
                     's3:ObjectOwnerOverrideToBucketOwner'
                 ],
                 'Resource': [
-                    'arn:aws:s3:::{0}'.format(args.dest),
-                    'arn:aws:s3:::{0}/*'.format(args.dest)
+                    'arn:aws:s3:::{0}'.format(dest_bucket),
+                    'arn:aws:s3:::{0}/*'.format(dest_bucket)
                 ]
             }
         ]
@@ -236,8 +328,8 @@ def main():
     source_bucket_policy = {}
     dest_iam_policy = {}
 
-    source = Bucket(args.source, args.src_region, args.src_profile, source_lifecycle_policy, source_iam_policy, source_bucket_policy)
-    dest = Bucket(args.dest, args.dest_region, args.dest_profile, dest_lifecycle_policy, dest_iam_policy, dest_bucket_policy)
+    source = Bucket(source_bucket, args.src_region, args.src_profile, source_lifecycle_policy, source_iam_policy, source_bucket_policy)
+    dest = Bucket(dest_bucket, args.dest_region, args.dest_profile, dest_lifecycle_policy, dest_iam_policy, dest_bucket_policy)
 
     for bucket in [source, dest]:
         bucket.create_bucket()
@@ -246,6 +338,9 @@ def main():
 
     dest.apply_security()
     source.create_iam_role()
+    source.enable_replication(dest.bucket['arn'])
+
+    logger.info('Setup is complete...')
 
 
 if __name__ == '__main__':
